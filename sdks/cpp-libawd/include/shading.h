8416f668b83d2949af4cbed4a63d81ea3d8e8221
Richard Olsson <r@richardolsson.se>
2011-07-25 21:39:10 +0200

    Implemented write prep methods for all libawd lighting/shadow shading methods.

--- sdks/cpp-libawd/include/shading.h
+++ sdks/cpp-libawd/include/shading.h
@@ -5,15 +5,79 @@
 #include "block.h"
 #include "attr.h"
 
+#define PROP_SHAD_STRENGTH 1
+#define PROP_SHAD_COLOR 2
+#define PROP_SHAD_ALPHA 3
+#define PROP_SHAD_MAP 4
+
+#define PROP_SHAD_ENVMAP_POSX 101
+#define PROP_SHAD_ENVMAP_NEGX 102
+#define PROP_SHAD_ENVMAP_POSY 103
+#define PROP_SHAD_ENVMAP_NEGY 104
+#define PROP_SHAD_ENVMAP_POSZ 105
+#define PROP_SHAD_ENVMAP_NEGZ 106
+
+#define PROP_SHAD_CEL_LEVELS 201
+#define PROP_SHAD_CEL_SMOOTHNESS 202
+#define PROP_SHAD_CEL_CUTOFF 203
+
+#define PROP_SHAD_SCATTERING 301
+#define PROP_SHAD_TRANSLUCENCY 302
+
+#define PROP_SHAD_GLOSS 21
+#define PROP_SHAD_GLOSS_MAP 22
+
+#define PROP_SHAD_NORMAL_REFLECTANCE 401
+
+#define PROP_SHAD_EPSILON 501
+#define PROP_SHAD_STEP_SIZE 502
+
+#define PROP_SHAD_COLOR_MATRIX 1001
+#define PROP_SHAD_ALPHA_MULT 1101
+#define PROP_SHAD_RED_MULT 1102
+#define PROP_SHAD_GREEN_MULT 1103
+#define PROP_SHAD_BLUE_MULT 1104
+#define PROP_SHAD_COLOR_OFFS 1105
+
+#define PROP_SHAD_FOG_DIST 1201
+
+#define PROP_SHAD_PROJ_MODE 1301
+
+#define PROP_SHAD_POWER 1401
+
+
+typedef enum {
+    AWD_SHADETYPE_BASIC_AMB=1,
+    AWD_SHADETYPE_ENVMAP_AMB,
+
+    AWD_SHADETYPE_BASIC_DIFF=101,
+    AWD_SHADETYPE_ENVMAP_DIFF,
+    AWD_SHADETYPE_CEL_DIFF,
+    AWD_SHADETYPE_SSS_DIFF,
+    AWD_SHADETYPE_TERRAIN_DIFF,
+
+    AWD_SHADETYPE_BASIC_SPEC=201,
+    AWD_SHADETYPE_CEL_SPEC,
+    AWD_SHADETYPE_FRESNEL_SPEC,
+
+    AWD_SHADETYPE_HARD_SHAD=301,
+    AWD_SHADETYPE_SOFT_SHAD,
+    AWD_SHADETYPE_FILTERED_SHAD,
+    AWD_SHADETYPE_SLOW_FILTERED_SHAD,
+
+    AWD_SHADETYPE_COLOR_MATRIX=401,
+    AWD_SHADETYPE_COLOR_TRANSFORM,
+    AWD_SHADETYPE_ENVMAP,
+    AWD_SHADETYPE_FOG,
+    AWD_SHADETYPE_PROJ_TEXTURE,
+    AWD_SHADETYPE_RIMLIGHT
+} AWD_shade_type;
+
 class AWDShadingMethod :
     public AWDAttrElement
 {
-    private:
-        // Type is defined by the sub-classes in their
-        // implementations of prepare_write().
-        awd_uint16 type;
-        
     protected:
+        AWD_shade_type type;
         virtual void prepare_write()=0;
 
     public:
@@ -25,7 +89,12 @@ class AWDShadingMethod :
 class AWDBasicAmbientMethod :
     public AWDShadingMethod
 {
+    protected:
+        void prepare_write();
+
     public:
+        AWDBasicAmbientMethod();
+
         awd_float64 strength;
         awd_color color;
 };
@@ -33,7 +102,12 @@ class AWDBasicAmbientMethod :
 class AWDEnvMapAmbientMethod :
     public AWDBasicAmbientMethod
 {
+    protected:
+        void prepare_write();
+
     public:
+        AWDEnvMapAmbientMethod();
+
         AWDTexture *posx_texture;
         AWDTexture *negx_texture;
         AWDTexture *posy_texture;
@@ -45,7 +119,12 @@ class AWDEnvMapAmbientMethod :
 class AWDBasicDiffuseMethod :
     public AWDShadingMethod
 {
+    protected:
+        void prepare_write();
+
     public:
+        AWDBasicDiffuseMethod();
+
         awd_color color;
         awd_float64 alpha;
         AWDTexture *diffuse_map;
@@ -54,15 +133,25 @@ class AWDBasicDiffuseMethod :
 class AWDCelDiffuseMethod :
     public AWDBasicDiffuseMethod
 {
+    protected:
+        void prepare_write();
+
     public:
-        awd_uint8 levels;
+        AWDCelDiffuseMethod();
+
+        awd_uint16 levels;
         awd_float64 smoothness;
 };
 
 class AWDEnvMapDiffuseMethod :
     public AWDBasicDiffuseMethod
 {
+    protected:
+        void prepare_write();
+
     public:
+        AWDEnvMapDiffuseMethod();
+
         AWDTexture *posx_texture;
         AWDTexture *negx_texture;
         AWDTexture *posy_texture;
@@ -74,8 +163,12 @@ class AWDEnvMapDiffuseMethod :
 class AWDSSSDiffuseMethod :
     public AWDBasicDiffuseMethod
 {
+    protected:
+        void prepare_write();
+
     public:
-        awd_color color;
+        AWDSSSDiffuseMethod();
+
         awd_float64 scattering;
         awd_float64 translucency;
 };
@@ -83,6 +176,8 @@ class AWDSSSDiffuseMethod :
 class AWDTerrainDiffuseMethod :
     public AWDBasicDiffuseMethod
 {
+    public:
+        AWDTerrainDiffuseMethod();
 };
 
 
@@ -90,7 +185,12 @@ class AWDTerrainDiffuseMethod :
 class AWDBasicSpecularMethod :
     public AWDShadingMethod
 {
+    protected:
+        void prepare_write();
+
     public:
+        AWDBasicSpecularMethod();
+
         awd_float64 strength;
         awd_color color;
         AWDTexture *specular_map;
@@ -101,7 +201,11 @@ class AWDBasicSpecularMethod :
 class AWDCelSpecularMethod :
     public AWDBasicSpecularMethod
 {
+    protected:
+        void prepare_write();
+
     public:
+        AWDCelSpecularMethod();
         awd_float64 smoothness;
         awd_float64 cutoff;
 };
@@ -109,7 +213,11 @@ class AWDCelSpecularMethod :
 class AWDFresnelSpecularMethod :
     public AWDBasicSpecularMethod
 {
+    protected:
+        void prepare_write();
+
     public:
+        AWDFresnelSpecularMethod();
         awd_float64 normal_reflectance;
 };
 
@@ -118,7 +226,12 @@ class AWDFresnelSpecularMethod :
 class AWDFilteredShadowMapMethod :
     public AWDShadingMethod
 {
+    protected:
+        void prepare_write();
+
     public:
+        AWDFilteredShadowMapMethod();
+
         awd_float64 epsilon;
         awd_float64 step_size;
 };
@@ -126,14 +239,24 @@ class AWDFilteredShadowMapMethod :
 class AWDHardShadowMapMethod :
     public AWDShadingMethod
 {
+    protected:
+        void prepare_write();
+
     public:
+        AWDHardShadowMapMethod();
+
         awd_float64 epsilon;
 };
 
 class AWDSoftShadowMapMethod :
     public AWDShadingMethod
 {
+    protected:
+        void prepare_write();
+
     public:
+        AWDSoftShadowMapMethod();
+
         awd_float64 epsilon;
         awd_float64 step_size;
 };
@@ -141,7 +264,12 @@ class AWDSoftShadowMapMethod :
 class AWDSlowFilteredShadowMapMethod :
     public AWDShadingMethod
 {
+    protected:
+        void prepare_write();
+
     public:
+        AWDSlowFilteredShadowMapMethod();
+
         awd_float64 epsilon;
         awd_float64 step_size;
 };
--- sdks/cpp-libawd/src/shading.cc
+++ sdks/cpp-libawd/src/shading.cc
@@ -3,6 +3,20 @@
 
 #include "platform.h"
 
+
+
+inline void __set_tex_prop(AWDNumAttrList *properties, awd_uint16 prop, AWDTexture *tex)
+{
+    if (tex) {
+        AWD_field_ptr val;
+        val.v = malloc(sizeof(awd_baddr));
+        *val.addr = tex->get_addr();
+        properties->set(prop, val, sizeof(awd_baddr), AWD_FIELD_BADDR);
+    }
+}
+
+
+
 awd_uint32
 AWDShadingMethod::calc_method_length(bool wide_geom, bool wide_mtx)
 {
@@ -20,3 +34,380 @@ AWDShadingMethod::write_method(int fd, bool wide_geom, bool wide_mtx)
     this->properties->write_attributes(fd, wide_geom, wide_mtx);
     this->user_attributes->write_attributes(fd, wide_geom, wide_mtx);
 }
+
+
+
+
+AWDBasicAmbientMethod::AWDBasicAmbientMethod()
+{
+    this->type = AWD_SHADETYPE_BASIC_AMB;
+    this->strength = 0.0f;
+    this->color = 0;
+}
+
+void
+AWDBasicAmbientMethod::prepare_write()
+{
+    if (this->strength != 0.0f) {
+        AWD_field_ptr val;
+        val.v = malloc(sizeof(awd_float64));
+        *val.f64 = this->strength;
+        this->properties->set(PROP_SHAD_STRENGTH, val, sizeof(awd_float64), AWD_FIELD_FLOAT64);
+    }
+    
+    if (this->color != 0) {
+        AWD_field_ptr val;
+        val.v = malloc(sizeof(awd_color));
+        *val.col = this->color;
+        this->properties->set(PROP_SHAD_COLOR, val, sizeof(awd_float64), AWD_FIELD_COLOR);
+    }
+}
+
+
+
+
+AWDEnvMapAmbientMethod::AWDEnvMapAmbientMethod()
+{
+    this->type = AWD_SHADETYPE_ENVMAP_AMB;
+    this->posx_texture = NULL;
+    this->negx_texture = NULL;
+    this->posy_texture = NULL;
+    this->negy_texture = NULL;
+    this->posz_texture = NULL;
+    this->negz_texture = NULL;
+}
+
+void
+AWDEnvMapAmbientMethod::prepare_write()
+{
+    AWDBasicAmbientMethod::prepare_write();
+
+    __set_tex_prop(properties, PROP_SHAD_ENVMAP_POSX, this->posx_texture);
+    __set_tex_prop(properties, PROP_SHAD_ENVMAP_NEGX, this->negx_texture);
+    __set_tex_prop(properties, PROP_SHAD_ENVMAP_POSY, this->posy_texture);
+    __set_tex_prop(properties, PROP_SHAD_ENVMAP_NEGY, this->negy_texture);
+    __set_tex_prop(properties, PROP_SHAD_ENVMAP_POSZ, this->posz_texture);
+    __set_tex_prop(properties, PROP_SHAD_ENVMAP_NEGZ, this->negz_texture);
+}
+
+
+
+AWDBasicDiffuseMethod::AWDBasicDiffuseMethod()
+{
+    this->type = AWD_SHADETYPE_BASIC_DIFF;
+    this->color = 0;
+    this->alpha = 1.0;
+    this->diffuse_map = NULL;
+}
+
+void
+AWDBasicDiffuseMethod::prepare_write()
+{
+    if (this->color != 0) {
+        AWD_field_ptr val;
+        val.v = malloc(sizeof(awd_color));
+        *val.col = this->color;
+        this->properties->set(PROP_SHAD_COLOR, val, sizeof(awd_color), AWD_FIELD_COLOR);
+    }
+
+    if (this->alpha != 1.0) {
+        AWD_field_ptr val;
+        val.v = malloc(sizeof(awd_float64));
+        *val.f64 = this->alpha;
+        this->properties->set(PROP_SHAD_ALPHA, val, sizeof(awd_float64), AWD_FIELD_FLOAT64);
+    }
+
+    __set_tex_prop(properties, PROP_SHAD_MAP, this->diffuse_map);
+}
+
+
+
+
+AWDCelDiffuseMethod::AWDCelDiffuseMethod()
+{
+    this->type = AWD_SHADETYPE_CEL_DIFF;
+    this->levels = 3;
+    this->smoothness = 0.1;
+}
+
+void
+AWDCelDiffuseMethod::prepare_write()
+{
+    AWDBasicDiffuseMethod::prepare_write();
+
+    if (this->levels != 3) {
+        AWD_field_ptr val;
+        val.v = malloc(sizeof(awd_uint16));
+        *val.ui16 = this->levels;
+        this->properties->set(PROP_SHAD_CEL_LEVELS, val, sizeof(awd_uint16), AWD_FIELD_UINT16);
+    }
+
+    if (this->smoothness != 0.1) {
+        AWD_field_ptr val;
+        val.v = malloc(sizeof(awd_float64));
+        *val.f64 = this->smoothness;
+        this->properties->set(PROP_SHAD_CEL_SMOOTHNESS, val, sizeof(awd_float64), AWD_FIELD_FLOAT64);
+    }
+}
+
+
+
+AWDEnvMapDiffuseMethod::AWDEnvMapDiffuseMethod()
+{
+    this->type = AWD_SHADETYPE_ENVMAP_DIFF;
+    this->posx_texture = NULL;
+    this->negx_texture = NULL;
+    this->posy_texture = NULL;
+    this->negy_texture = NULL;
+    this->posz_texture = NULL;
+    this->negz_texture = NULL;
+}
+
+void
+AWDEnvMapDiffuseMethod::prepare_write()
+{
+    AWDBasicDiffuseMethod::prepare_write();
+
+    __set_tex_prop(properties, PROP_SHAD_ENVMAP_POSX, this->posx_texture);
+    __set_tex_prop(properties, PROP_SHAD_ENVMAP_NEGX, this->negx_texture);
+    __set_tex_prop(properties, PROP_SHAD_ENVMAP_POSY, this->posy_texture);
+    __set_tex_prop(properties, PROP_SHAD_ENVMAP_NEGY, this->negy_texture);
+    __set_tex_prop(properties, PROP_SHAD_ENVMAP_POSZ, this->posz_texture);
+    __set_tex_prop(properties, PROP_SHAD_ENVMAP_NEGZ, this->negz_texture);
+}
+
+
+
+
+AWDSSSDiffuseMethod::AWDSSSDiffuseMethod()
+{
+    this->type = AWD_SHADETYPE_SSS_DIFF;
+    this->scattering = 0.2;
+    this->translucency = 1.0;
+}
+
+void
+AWDSSSDiffuseMethod::prepare_write()
+{
+    AWDBasicDiffuseMethod::prepare_write();
+
+    if (this->scattering != 0.2) {
+        AWD_field_ptr val;
+        val.v = malloc(sizeof(awd_float64));
+        *val.f64 = this->scattering;
+        this->properties->set(PROP_SHAD_SCATTERING, val, sizeof(awd_float64), AWD_FIELD_FLOAT64);
+    }
+
+    if (this->translucency != 1.0) {
+        AWD_field_ptr val;
+        val.v = malloc(sizeof(awd_float64));
+        *val.f64 = this->translucency;
+        this->properties->set(PROP_SHAD_TRANSLUCENCY, val, sizeof(awd_float64), AWD_FIELD_FLOAT64);
+    }
+}
+
+
+
+
+AWDTerrainDiffuseMethod::AWDTerrainDiffuseMethod()
+{
+    this->type = AWD_SHADETYPE_TERRAIN_DIFF;
+}
+
+
+
+
+AWDBasicSpecularMethod::AWDBasicSpecularMethod()
+{
+    this->type = AWD_SHADETYPE_BASIC_SPEC;
+    this->strength = 1.0;
+    this->color = 0xffffff;
+    this->gloss = 50.0;
+    this->specular_map = NULL;
+    this->gloss_map = NULL;
+}
+
+void
+AWDBasicSpecularMethod::prepare_write()
+{
+    if (this->strength != 1.0) {
+        AWD_field_ptr val;
+        val.v = malloc(sizeof(awd_float64));
+        *val.f64 = this->strength;
+        this->properties->set(PROP_SHAD_STRENGTH, val, sizeof(awd_float64), AWD_FIELD_FLOAT64);
+    }
+
+    if (this->color != 0xffffff) {
+        AWD_field_ptr val;
+        val.v = malloc(sizeof(awd_color));
+        *val.col = this->color;
+        this->properties->set(PROP_SHAD_COLOR, val, sizeof(awd_color), AWD_FIELD_COLOR);
+    }
+
+    if (this->gloss != 50.0) {
+        AWD_field_ptr val;
+        val.v = malloc(sizeof(awd_float64));
+        *val.f64 = this->gloss;
+        this->properties->set(PROP_SHAD_GLOSS, val, sizeof(awd_float64), AWD_FIELD_FLOAT64);
+    }
+
+    __set_tex_prop(properties, PROP_SHAD_MAP, this->specular_map);
+    __set_tex_prop(properties, PROP_SHAD_GLOSS_MAP, this->gloss_map);
+}
+
+
+
+
+AWDCelSpecularMethod::AWDCelSpecularMethod()
+{
+    this->type = AWD_SHADETYPE_CEL_SPEC;
+    this->smoothness = 0.1;
+    this->cutoff = 0.5;
+}
+
+void
+AWDCelSpecularMethod::prepare_write()
+{
+    AWDBasicSpecularMethod::prepare_write();
+
+    if (this->smoothness != 0.1) {
+        AWD_field_ptr val;
+        val.v = malloc(sizeof(awd_float64));
+        *val.f64 = this->smoothness;
+        this->properties->set(PROP_SHAD_CEL_SMOOTHNESS, val, sizeof(awd_float64), AWD_FIELD_FLOAT64);
+    }
+
+    if (this->cutoff != 0.5) {
+        AWD_field_ptr val;
+        val.v = malloc(sizeof(awd_float64));
+        *val.f64 = this->cutoff;
+        this->properties->set(PROP_SHAD_CEL_CUTOFF, val, sizeof(awd_float64), AWD_FIELD_FLOAT64);
+    }
+}
+
+
+
+
+AWDFresnelSpecularMethod::AWDFresnelSpecularMethod()
+{
+    this->type = AWD_SHADETYPE_FRESNEL_SPEC;
+    this->normal_reflectance = 0.028;
+}
+
+void
+AWDFresnelSpecularMethod::prepare_write()
+{
+    AWDBasicSpecularMethod::prepare_write();
+
+    if (this->normal_reflectance != 0.028) {
+        AWD_field_ptr val;
+        val.v = malloc(sizeof(awd_float64));
+        *val.f64 = this->normal_reflectance;
+        this->properties->set(PROP_SHAD_NORMAL_REFLECTANCE, val, sizeof(awd_float64), AWD_FIELD_FLOAT64);
+    }
+}
+
+
+
+
+AWDFilteredShadowMapMethod::AWDFilteredShadowMapMethod()
+{
+    this->type = AWD_SHADETYPE_FILTERED_SHAD;
+    this->epsilon = 0.003;
+    this->step_size = 0.0;
+}
+
+void
+AWDFilteredShadowMapMethod::prepare_write()
+{
+    if (this->epsilon != 0.003) {
+        AWD_field_ptr val;
+        val.v = malloc(sizeof(awd_float64));
+        *val.f64 = this->epsilon;
+        this->properties->set(PROP_SHAD_EPSILON, val, sizeof(awd_float64), AWD_FIELD_FLOAT64);
+    }
+
+    if (this->step_size != 0.0) {
+        AWD_field_ptr val;
+        val.v = malloc(sizeof(awd_float64));
+        *val.f64 = this->step_size;
+        this->properties->set(PROP_SHAD_STEP_SIZE, val, sizeof(awd_float64), AWD_FIELD_FLOAT64);
+    }
+}
+
+
+
+
+AWDHardShadowMapMethod::AWDHardShadowMapMethod()
+{
+    this->type = AWD_SHADETYPE_HARD_SHAD;
+    this->epsilon = 0.003;
+}
+
+void
+AWDHardShadowMapMethod::prepare_write()
+{
+    if (this->epsilon != 0.003) {
+        AWD_field_ptr val;
+        val.v = malloc(sizeof(awd_float64));
+        *val.f64 = this->epsilon;
+        this->properties->set(PROP_SHAD_EPSILON, val, sizeof(awd_float64), AWD_FIELD_FLOAT64);
+    }
+}
+
+
+
+
+AWDSoftShadowMapMethod::AWDSoftShadowMapMethod()
+{
+    this->type = AWD_SHADETYPE_SOFT_SHAD;
+    this->epsilon = 0.003;
+    this->step_size = 0.0;
+}
+
+void
+AWDSoftShadowMapMethod::prepare_write()
+{
+    if (this->epsilon != 0.003) {
+        AWD_field_ptr val;
+        val.v = malloc(sizeof(awd_float64));
+        *val.f64 = this->epsilon;
+        this->properties->set(PROP_SHAD_EPSILON, val, sizeof(awd_float64), AWD_FIELD_FLOAT64);
+    }
+
+    if (this->step_size != 0.0) {
+        AWD_field_ptr val;
+        val.v = malloc(sizeof(awd_float64));
+        *val.f64 = this->step_size;
+        this->properties->set(PROP_SHAD_STEP_SIZE, val, sizeof(awd_float64), AWD_FIELD_FLOAT64);
+    }
+}
+
+
+
+
+AWDSlowFilteredShadowMapMethod::AWDSlowFilteredShadowMapMethod()
+{
+    this->type = AWD_SHADETYPE_SLOW_FILTERED_SHAD;
+    this->epsilon = 0.003;
+    this->step_size = 0.0;
+}
+
+void
+AWDSlowFilteredShadowMapMethod::prepare_write()
+{
+    if (this->epsilon != 0.003) {
+        AWD_field_ptr val;
+        val.v = malloc(sizeof(awd_float64));
+        *val.f64 = this->epsilon;
+        this->properties->set(PROP_SHAD_EPSILON, val, sizeof(awd_float64), AWD_FIELD_FLOAT64);
+    }
+
+    if (this->step_size != 0.0) {
+        AWD_field_ptr val;
+        val.v = malloc(sizeof(awd_float64));
+        *val.f64 = this->step_size;
+        this->properties->set(PROP_SHAD_STEP_SIZE, val, sizeof(awd_float64), AWD_FIELD_FLOAT64);
+    }
+}
+
