66c1e85192032d92b668da6bb08d7bff40f2dffb
Richard Olsson <Richard Olsson@localhost>
2012-04-15 14:29:06 +0200

    Fixed major issues with data stream writing.

--- sdks/cpp-libawd/src/stream.cc
+++ sdks/cpp-libawd/src/stream.cc
@@ -65,49 +65,49 @@ AWDDataStream::write_stream(int fd)
             write(fd, &elem, sizeof(awd_int8));
         }
     }
-    else if (this->type == AWD_FIELD_INT16) {
+    else if (this->data_type == AWD_FIELD_INT16) {
         for (e=0; e<num; e++) {
             awd_int32 *p = (this->data.i32 + e);
             awd_int16 elem = UI16((awd_int16)*p);
             write(fd, &elem, sizeof(awd_int16));
         }
     }
-    else if (this->type == AWD_FIELD_INT32) {
+    else if (this->data_type == AWD_FIELD_INT32) {
         for (e=0; e<num; e++) {
             awd_int32 *p = (this->data.i32 + e);
             awd_int32 elem = UI32((awd_int32)*p);
             write(fd, &elem, sizeof(awd_int32));
         }
     }
-    else if (this->type == AWD_FIELD_UINT8) {
+    else if (this->data_type == AWD_FIELD_UINT8) {
         for (e=0; e<num; e++) {
             awd_uint32 *p = (this->data.ui32 + e);
             awd_uint8 elem = (awd_uint8)*p;
             write(fd, &elem, sizeof(awd_uint8));
         }
     }
-    else if (this->type == AWD_FIELD_UINT16) {
+    else if (this->data_type == AWD_FIELD_UINT16) {
         for (e=0; e<num; e++) {
             awd_uint32 *p = (this->data.ui32 + e);
-            awd_uint16 elem = UI16((awd_uint8)*p);
-            write(fd, &elem, sizeof(awd_uint8));
+            awd_uint16 elem = UI16((awd_uint16)*p);
+            write(fd, &elem, sizeof(awd_uint16));
         }
     }
-    else if (this->type == AWD_FIELD_UINT32) {
+    else if (this->data_type == AWD_FIELD_UINT32) {
         for (e=0; e<num; e++) {
             awd_uint32 *p = (this->data.ui32 + e);
             awd_uint32 elem = UI32((awd_uint32)*p);
             write(fd, &elem, sizeof(awd_uint32));
         }
     }
-    else if (this->type == AWD_FIELD_FLOAT32) {
+    else if (this->data_type == AWD_FIELD_FLOAT32) {
         for (e=0; e<num; e++) {
             awd_float64 *p = (this->data.f64 + e);
             awd_float32 elem = F32((awd_float32)*p);
             write(fd, &elem, sizeof(awd_float32));
         }
     }
-    else if (this->type == AWD_FIELD_FLOAT64) {
+    else if (this->data_type == AWD_FIELD_FLOAT64) {
         for (e=0; e<num; e++) {
             awd_float64 *p = (this->data.f64 + e);
             awd_float64 elem = F64((awd_float64)*p);
