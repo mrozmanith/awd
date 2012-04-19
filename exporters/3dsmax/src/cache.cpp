43c4bc0dfe2c70db9d16be22548a32757d3bfcd6
Richard Olsson <Richard Olsson@localhost>
2012-04-19 14:23:42 +0200

    Implemented caching of materials to enable re-use of material blocks.

--- exporters/3dsmax/src/cache.cpp
+++ exporters/3dsmax/src/cache.cpp
@@ -10,6 +10,7 @@ BlockCache::BlockCache(void)
 
 BlockCache::~BlockCache(void)
 {
+	// TODO: Free all items
 }
 
 void BlockCache::Set(void *key, void *val)
@@ -39,6 +40,54 @@ void *BlockCache::Get(void *key)
 	while (cur) {
 		if (cur->key == key)
 			return cur->val;
+
+		cur = cur->next;
+	}
+
+	return NULL;
+}
+
+
+
+ColorMaterialCache::ColorMaterialCache(void)
+{
+	firstItem = NULL;
+	lastItem = NULL;
+}
+
+
+ColorMaterialCache::~ColorMaterialCache(void)
+{
+	// TODO: Free all items
+}
+
+void ColorMaterialCache::Set(awd_color color, AWDMaterial *mtl)
+{
+	ColorMaterialCacheItem *item;
+
+	item = (ColorMaterialCacheItem *)malloc(sizeof(ColorMaterialCacheItem));
+	item->color = color;
+	item->mtl = mtl;
+	item->next = NULL;
+
+	if (!firstItem) {
+		firstItem = item;
+	}
+	else {
+		lastItem->next = item;
+	}
+
+	lastItem = item;
+}
+
+AWDMaterial *ColorMaterialCache::Get(awd_color color)
+{
+	ColorMaterialCacheItem *cur;
+
+	cur = firstItem;
+	while (cur) {
+		if (cur->color == color)
+			return cur->mtl;
 
 		cur = cur->next;
 	}
--- exporters/3dsmax/src/cache.h
+++ exporters/3dsmax/src/cache.h
@@ -1,5 +1,12 @@
 #pragma once
 
+#include "awd/awd.h"
+
+/**
+ * BlockCache
+ * Cache that is used generically to store any type of block or 
+ * sub-element in a look-up table by MAX object.
+*/
 typedef struct BlockCacheItem_struct {
 	void *key;
 	void *val;
@@ -20,3 +27,29 @@ public:
 	void *Get(void *key);
 };
 
+
+/**
+ * ColorMaterialCache
+ * Cache that is used for "default" color materials applied to blocks
+ * that have no material assigned. AWDMaterial blocks are stored in a
+ * dictionary cache by color.
+*/
+typedef struct ColorMaterialCacheItem_struct {
+	awd_color color;
+	AWDMaterial *mtl;
+	ColorMaterialCacheItem_struct *next;
+} ColorMaterialCacheItem;
+
+class ColorMaterialCache
+{
+private:
+	ColorMaterialCacheItem *firstItem;
+	ColorMaterialCacheItem *lastItem;
+
+public:
+	ColorMaterialCache(void);
+	~ColorMaterialCache(void);
+
+	void Set(awd_color color, AWDMaterial *mtl);
+	AWDMaterial *Get(awd_color color);
+};
